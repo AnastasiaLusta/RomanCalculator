035038a1f8c8e5b4aed04eefbecddb5f11521acb
AnastasiaLusta <itssialusta@gmail.com>
2022-09-02 12:50:55 +0300

    feat: add romannumber class

---
+++ RomanCalculator/App/RomanNumber.cs
@@ -0,0 +1,38 @@
+namespace RomanCalculator.App;
+
+public class RomanNumber
+{
+    public static int Parse(string romanNumber)
+    {
+        var digits = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+        var result = 0;
+        var previous = 0;
+        
+        foreach (var number in romanNumber)
+        {
+            var current = digits[number];
+            if (current > previous)
+            {
+                result -= previous;
+                result += current - previous;
+            }
+            else
+            {
+                result+= digits[number];
+            }
+
+            previous = current;
+        }
+
+        return result;
+    }
+}
