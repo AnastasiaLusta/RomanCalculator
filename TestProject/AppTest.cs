8ed60de384cd153c73cd1a05d8efc870aec4b623
AnastasiaLusta <itssialusta@gmail.com>
2022-09-02 12:52:36 +0300

    feat: add tests for parsing romannumber to arabic numerals

--- TestProject/AppTest.cs
+++ TestProject/AppTest.cs
@@ -1,10 +1,27 @@
-namespace TestProject;
+namespace RomanCalculator.App;
 
 [TestClass]
 public class AppTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void CalcTest()
     {
+        Calc calc = new();
+        Assert.IsNotNull(calc);
+    }
+
+    [TestMethod]
+    public void RomanNumberParseTest()
+    {
+        Assert.AreEqual(RomanNumber.Parse("I"), 1, "I == 1");
+        Assert.AreEqual(RomanNumber.Parse("IV"), 4, "IV == 4");
+        Assert.AreEqual(RomanNumber.Parse("XV"), 15);
+        Assert.AreEqual(RomanNumber.Parse("XXX"), 30);
+        Assert.AreEqual(RomanNumber.Parse("CM"), 900);
+        Assert.AreEqual(RomanNumber.Parse("MCMXCIX"), 1999);
+        Assert.AreEqual(RomanNumber.Parse("CD"), 400);
+        Assert.AreEqual(RomanNumber.Parse("CDI"), 401);
+        Assert.AreEqual(RomanNumber.Parse("LV"), 55);
+        Assert.AreEqual(RomanNumber.Parse("XL"), 40);
     }
 }
