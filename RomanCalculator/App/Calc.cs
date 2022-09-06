d58b935273b51b9bba290e057e9305b335b32380
ddsinyakov <dimasiniyakovwork@gmail.com>
2022-09-06 09:59:51 +0300

    Console.WriteLine in Calc.Run to show the RomanNumber.Parse results

--- RomanCalculator/App/Calc.cs
+++ RomanCalculator/App/Calc.cs
@@ -5,5 +5,7 @@ public class Calc
     public void Run()
     {
         Console.WriteLine("Calculator is running");
+        Console.WriteLine(RomanNumber.Parse("CM"));
+        Console.WriteLine(RomanNumber.Parse("CD"));
     }
 }
