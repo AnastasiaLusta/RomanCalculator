9857e09e570f8ec861e8cb54b7e76c262a8d6e7d
AnastasiaLusta <itssialusta@gmail.com>
2022-09-18 22:49:24 +0300

    test: add test for mult and div operations

--- TestProject/AppTest.cs
+++ TestProject/AppTest.cs
@@ -21,8 +21,12 @@ public class AppTest
     {
         Calc calc = new(Resources);
         Assert.IsNotNull(calc.EvalExpression("XI + IV"));
+        Assert.IsNotNull(calc.EvalExpression("XI - IV"));
+        Assert.IsNotNull(calc.EvalExpression("XI * IV"));
+        Assert.IsNotNull(calc.EvalExpression("XI / IV"));
         Assert.AreEqual(new RomanNumber(10), calc.EvalExpression("XI - I"));
         Assert.ThrowsException<ArgumentException>(() => calc.EvalExpression("2 + 3"));
+        
     }
 
     [TestMethod]
